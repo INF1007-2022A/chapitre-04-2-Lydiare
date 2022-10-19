e7bdadbf6c2171effc84098f7f689fd6b1a21339
Lydiare <73038866+Lydiare@users.noreply.github.com>
2022-10-18 23:15:54 -0400

    exercices

--- exercice.py
+++ exercice.py
@@ -4,12 +4,28 @@
 import random
 
 def get_first_part_of_name(name):
-	return ""
+	# le nom est séparé lorsqu'il trouve '-' et chacun des nom est dans une liste
+	name = name.split('-')[0]
+
+
+	#Mettre majuscule la première lettre et minuscule les autres
+	mise_en_forme = name[0].capitalize() + name[1:].lower()
+
+	return f"Bonjour {mise_en_forme}"
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+	phrase = 'Aujourd’hui, j’ai vu un %s s’emparer d’un panier %s plein de %s'
+
+	animal_word = animals[random.randrange(0, len(animals))]
+	adject_word = adjectives[random.randrange(0, len(adjectives))]
+	fruits_word = fruits[random.randrange(0, len(fruits))]
+
+	return f"Aujourd’hui, j’ai vu un {animal_word} s’emparer d’un panier {adject_word} plein de {fruits_word}"
 
 def encrypt(text, shift):
+
+	code_cryptage = 
+	print(text)
 	return ""
 
 def decrypt(encrypted_text, shift):
